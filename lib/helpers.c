5b29d426fff12eea3ecc615351bf18d93152ba5a
Snopi <snopiashe4ka@gmail.com>
2015-03-18 00:01:30 +0300

    Formatting fixed. Read_until implemented. Makefile for revwords added.

--- lib/helpers.c
+++ lib/helpers.c
@@ -6,14 +6,14 @@ ssize_t read_(int fd, void *buf, size_t count) {
     ssize_t ret_value;
     char *char_buf = (char *) buf;
     while (1) {
-	ret_value = read(fd, char_buf + offset, count - offset);
-	if (ret_value < 0) {
-	    return ret_value;
-	}
-	if (ret_value == 0) {
-	    return offset;
-	}
-	offset += ret_value;
+		ret_value = read(fd, char_buf + offset, count - offset);
+		if (ret_value < 0) {
+	  		return ret_value;
+		}
+		if (ret_value == 0) {
+		    return offset;
+		}
+		offset += ret_value;
     }
 }
 
@@ -22,15 +22,29 @@ ssize_t write_(int fd, const void *buf, size_t count) {
     ssize_t ret_value;
     char *char_buf = (char *) buf;
     while (1) {
-	ret_value = write(fd, char_buf + offset, count - offset);
-	if (ret_value < 0) {
-	    return ret_value;
-	}
-	offset += ret_value;
-	if (offset >= count) {
-	    return offset;
-	}
+		ret_value = write(fd, char_buf + offset, count - offset);
+		if (ret_value < 0) {
+			return ret_value;
+		}
+		offset += ret_value;
+		if (offset >= count) {
+		  	return offset;
+		}
     }
 }
 
-
+ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
+	size_t offset = 0;
+	ssize_t ret_value;
+	char *char_buf = (char *) buf;
+	while (1) {
+		ret_value = read(fd, char_buf + offset, 1);
+		if (ret_value < 0) {
+			return ret_value;
+		}
+		offset += ret_value;
+		if (ret_value == 0 || char_buf[offset - 1] == delimiter) {
+			return offset;
+		}
+	}
+}
