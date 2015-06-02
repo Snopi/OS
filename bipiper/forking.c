19cd8124e8e4ef81d1ed712ad7a63b512405e7b9
Snopi <Snopiashe4ka@gmail.com>
2015-06-03 01:02:57 +0300

    Some useless spaces removed

--- bipiper/forking.c
+++ bipiper/forking.c
@@ -1,4 +1,3 @@
-//getaddrinfo includes
 #define _GNU_SOURCE
 #include <signal.h>
 #include <sys/types.h>
@@ -12,7 +11,7 @@
 #include <strings.h>
 #include <stdlib.h>
 
-#define BUF_SIZE 1024 
+#define BUF_SIZE 1024
 #define LISTEN_THRESHOLD 100
 #define PERROR_AND_EXIT(a) { perror(a); exit(EXIT_FAILURE);}
 
@@ -20,7 +19,7 @@ int make_serv_sock(char *port) {
     struct addrinfo hints;
     struct addrinfo *result, *rp;
     bzero(&hints, sizeof hints);
-    
+
     hints.ai_family = AF_INET; // Ipv4
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE; //bind
@@ -34,20 +33,20 @@ int make_serv_sock(char *port) {
 
     int serv_sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (serv_sock == -1)
-        PERROR_AND_EXIT("Serv_socket"); 
-    
+        PERROR_AND_EXIT("Serv_socket");
+
     int one = 1;
     if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int)) == -1)
         PERROR_AND_EXIT("setsockopt");
 
     if (bind(serv_sock, result->ai_addr, result->ai_addrlen))
-        PERROR_AND_EXIT("bind"); 
-    
+        PERROR_AND_EXIT("bind");
+
     freeaddrinfo(result);
-    
+
     if (listen(serv_sock, LISTEN_THRESHOLD))
-        PERROR_AND_EXIT("listen"); 
-    
+        PERROR_AND_EXIT("listen");
+
     return serv_sock;
 }
 
@@ -63,36 +62,36 @@ int main(int argc, char **argv) {
     sa.sa_flags = SA_RESTART;
     if (sigaction(SIGCHLD, &sa, NULL) < 0)
         return -1;
- 
+
     int serv_sock_1 = make_serv_sock(argv[1]);
     int serv_sock_2 = make_serv_sock(argv[2]);
-       
+
     struct buf_t *b = buf_new(BUF_SIZE);//nifty buffer
     while (1) {
         struct sockaddr_in client;
         socklen_t sz1 = sizeof client;
-        int cli1 = accept(serv_sock_1, (struct sockaddr*)&client, &sz1); 
-   
+        int cli1 = accept(serv_sock_1, (struct sockaddr*)&client, &sz1);
+
         struct sockaddr_in client2;
-        socklen_t sz2 = sizeof client2; 
-        int cli2 = accept(serv_sock_2, (struct sockaddr*)&client2, &sz2); 
-  
+        socklen_t sz2 = sizeof client2;
+        int cli2 = accept(serv_sock_2, (struct sockaddr*)&client2, &sz2);
+
         int pid1 = fork();
         if (!pid1) {//from cli1 to cli2
             while (buf_fill(cli1, b, 1) > 0)
                 buf_flush(cli2, b, b->size);
-            return 0;  
+            return 0;
         }
 
         int pid2 = fork();
         if (!pid2) {//from cli2 to cli1
             while (buf_fill(cli2, b, 1) > 0)
                 buf_flush(cli1, b, b->size);
-            return 0;  
+            return 0;
         }
 
         close(cli1); //close sockets on server
-        close(cli2); 
+        close(cli2);
     }
     return 0;
 }
--- filesender/filesender.c
+++ filesender/filesender.c
@@ -12,7 +12,7 @@
 #include <signal.h>
 
 
-#define BUF_SIZE 1024 
+#define BUF_SIZE 1024
 #define LISTEN_THRESHOLD 100
 #define PERROR_AND_EXIT(a) { perror(a); exit(EXIT_FAILURE);}
 
@@ -22,18 +22,18 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Usage: %s port file\n", argv[0]);
         return 0;
     }
-    
+
     struct sigaction sa; //Don't wait for children
     bzero(&sa, sizeof(sa));
     sa.sa_handler = SIG_IGN;
     sa.sa_flags = SA_RESTART;
     if (sigaction(SIGCHLD, &sa, NULL) < 0)
         return -1;
- 
+
     struct addrinfo hints;
     struct addrinfo *result, *rp;
     bzero(&hints, sizeof hints);
-    
+
     hints.ai_family = AF_INET; // Ipv4
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE; //bind
@@ -46,27 +46,26 @@ int main(int argc, char **argv) {
     }
 
     int serv_sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (serv_sock == -1) 
-        PERROR_AND_EXIT("Serv_socket"); 
+    if (serv_sock == -1)
+        PERROR_AND_EXIT("Serv_socket");
 
     int one = 1;
     if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int)) == -1)
-        PERROR_AND_EXIT("setsockopt");   
+        PERROR_AND_EXIT("setsockopt");
 
     if (bind(serv_sock, result->ai_addr, result->ai_addrlen))
-        PERROR_AND_EXIT("bind"); 
-    
+        PERROR_AND_EXIT("bind");
+
     freeaddrinfo(result);
     //we can use SOMAXCONN here instead of LISTEN_THRESHOLD
-    if (listen(serv_sock, LISTEN_THRESHOLD)) 
-        PERROR_AND_EXIT("listen"); 
-
+    if (listen(serv_sock, LISTEN_THRESHOLD))
+        PERROR_AND_EXIT("listen");
     int fd;
     while(1) {
         struct sockaddr_in client;
         socklen_t sz = sizeof client;
-        fd = accept(serv_sock, (struct sockaddr*)&client, &sz); 
-        
+        fd = accept(serv_sock, (struct sockaddr*)&client, &sz);
+
         if (fork()) {
             close(fd);
         } else {
@@ -76,16 +75,17 @@ int main(int argc, char **argv) {
                 PERROR_AND_EXIT("fileopen");
             }
             struct buf_t *b = buf_new(BUF_SIZE);
-            int success = 1;
-            while(buf_fill(file, b, 1) > 0) 
-                buf_flush(fd, b, b->size); //if something goes wrong
-               // it will recieve SIGPIPE and die
+
+            while(buf_fill(file, b, 1) > 0)
+                buf_flush(fd, b, b->size);
+            //if something goes wrong
+            // it will recieve SIGPIPE and die
             buf_free(b);//unnecessary actions..
             close(file);
             close(fd);
-            exit(EXIT_SUCCESS); 
+            exit(EXIT_SUCCESS);
         }
-    }  
+    }
 
     return 0;
 }
